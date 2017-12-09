a8edacfe4de6956fd1dda4c4134bf603af4184d7
Scott Faber <scottfaberphoto@gmail.com>
2017-12-09 00:34:19 -0500

    add changes to connection js to be able to huck up with Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,25 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "1234",
-	database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "1234",
+// 	database: "burgers_db"
+// });
+var connection;
+
+if (process.env.JAWSBD_URL) {
+	connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '1234',
+		database: 'burgers_db'
+
+	});
+};
+
+
 
 connection.connect(function(err){
 	if (err) {
