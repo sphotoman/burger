43536a642acf2721022800f46b77c18750a800ed
Scott Faber <scottfaberphoto@gmail.com>
2017-12-08 22:25:50 -0500

    post tutoring... alot working... need to do a few more things

--- server.js
+++ server.js
@@ -13,8 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.use(methodOverride("_method"));
+
 // Static directory
 app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
